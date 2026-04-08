c02842b23e4d841635338d879373c3e4ff850710
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-04-09 01:26:21 +0200

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_40_Linq02.Models
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+        public string Major { get; set; }
+    }
+}
