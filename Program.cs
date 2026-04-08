f9c4667e19e5d0e31d0bed8136d5e551bb3adaa7
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-04-09 01:32:55 +0200

    Question 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace G_Net_40_Linq02
+﻿using G_Net_40_Linq02.Datasources;
+
+namespace G_Net_40_Linq02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-#
+            #region Question 1
+            var top = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+            #endregion
         }
     }
 }
