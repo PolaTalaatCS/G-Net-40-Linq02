7379e9458f49975c8ca80b20c6207b9b1d8138cb
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-04-09 01:36:34 +0200

    Question 3

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace G_Net_40_Linq02
             #region Question 1
             var top = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
             #endregion
+            #region Question 3
+            var list3 = Source.ProductList.TakeWhile(p => p.UnitPrice < 25);
+            #endregion
         }
     }
 }
