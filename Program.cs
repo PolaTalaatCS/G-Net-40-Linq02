50bd3c0ac12c488d973e7994ba88f19fbf906ff0
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-04-09 01:59:46 +0200

    Question 8

--- Program.cs
+++ Program.cs
@@ -1,39 +1,7 @@
-﻿using G_Net_40_Linq02.Datasources;
+﻿using G_Net_40_Linq02.Datasources;namespace G_Net_40_Linq02{    internal class Program    {        static void Main(string[] args)        {            #region Question 1            var top = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);            #endregion            #region Question 3            var list3 = Source.ProductList.TakeWhile(p => p.UnitPrice < 25);            #endregion            #region Question 4            var check = Source.ProductList.All(p => p.Category == "Seafood");            #endregion            #region Question 5            int[] ids = { 3, 9, 13, 18 };            var contains = ids.Contains(9);            #endregion            #region Question 6            var groupping = Source.ProductList.GroupBy(p => p.Category);            foreach (var group in groupping)            {                Console.WriteLine($"group : {group.Key} Count = {group.Count()}");            }            #endregion            #region Question 7            var groupping1= Source.ProductList.GroupBy(p => p.Category).Select(g => new     {         Category = g.Key,         ProductNames = g.Select(p => p.ProductName)     });
 
-namespace G_Net_40_Linq02
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            #region Question 1
-            var top = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
-            #endregion
-            #region Question 3
-            var list3 = Source.ProductList.TakeWhile(p => p.UnitPrice < 25);
-            #endregion
-            #region Question 4
-            var check = Source.ProductList.All(p => p.Category == "Seafood");
 
             #endregion
-            #region Question 5
-            int[] ids = { 3, 9, 13, 18 };
-            var contains = ids.Contains(9);
-            #endregion
-            #region Question 6
-            var groupping = Source.ProductList.GroupBy(p => p.Category);
-            foreach (var group in groupping)
-            {
-                Console.WriteLine($"group : {group.Key} Count = {group.Count()}");
-            }
-            #endregion
-            #region Question 7
-            var groupping1= Source.ProductList.GroupBy(p => p.Category).Select(g => new
-     {
-         Category = g.Key,
-         ProductNames = g.Select(p => p.ProductName)
-     });
-            #endregion
-        }
-    }
-}
+            #region Question 8      var result = Source.ProductList.GroupBy(p => p.Category).Where(g => g.Count() > 3);
+          #endregion
+        }    }}
